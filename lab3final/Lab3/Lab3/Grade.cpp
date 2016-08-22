fe3a97f8cafbf13a3fd8689e3f4fdeb085a0829a
mark52551 <mark.ruiz5@gmail.com>
2016-08-22 19:15:23 -0400

    commit changes

--- lab3final/Lab3/Lab3/Grade.cpp
+++ lab3final/Lab3/Lab3/Grade.cpp
@@ -2,7 +2,7 @@
 
 double grade::PrintGrade(int quiz_1, int quiz_2, int midterm, int finalexam)
 {
-	double quizzes = ((((((quiz_1 + quiz_2) / 20.0)*100.0)*.25) + midterm* 0.25) + finalexam *0.50);
+	double quizzes = (((((((quiz_1/10.0)+ (quiz_2/10.0))/2.0)*100.0)*0.25) + midterm* 0.25) + finalexam *0.50);
 	return quizzes;
 }
 
